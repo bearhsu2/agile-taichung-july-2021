0601b7d18908810d233625e7cafde4b0286c675c
yuanyu90221 <yuanyu90221@gmail.com>
2021-07-03 10:36:38 +0800

    add randomGetMoney

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public static int randomGetMoney(int originMoney, double p) {
+        int result = 0;
+//        int ratio = 0;
+        if (p  < 0.09) {
+            result = originMoney * 2;
+        } else if (p < 0.01) {
+            result = originMoney * 5;
+        }
+        return result;
+    }
+    public static double genRandom() {
+        return Math.random();
+    }
 }
